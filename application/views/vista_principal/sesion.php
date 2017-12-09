3aea8094b570d681eeb6e4f8eeddf93ad74bad6c
miguelort095 <miguelort095@gmail.com>
2017-12-09 15:49:22 -0400

    nav

---
+++ application/views/vista_principal/sesion.php
@@ -0,0 +1,7 @@
+<div class="container">
+  <div class="row">
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+  </div>
+</div>
