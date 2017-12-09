e56fe084e4e5a0d5c38e2b1aa2bc8b40975578aa
SamuelCampo <31445815+SamuelCampo@users.noreply.github.com>
2017-12-09 13:56:35 -0430

    Archivo inicial del login

---
+++ application/views/vista_principal/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="<?php echo base_url('asset/css/bootstrap.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('asset/css/bootstrap-material-design.min.css') ?>">
+    <!-- Animation library for notifications   -->
+    <link href="<?php echo base_url('asset/css/animate.min.css') ?>" rel="stylesheet"/>
+    <link rel="stylesheet" href="<?php echo base_url('asset/css/style.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('asset/css/easy-autocomplete.css') ?>"> 
+	<title>CRYPTOFUTURE|</title>
+</head>
+<body>
+	
+</body>
+</html>
